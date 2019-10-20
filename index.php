0e2c44b7d95f3e32e173d9d1de260d13597d60bd
Victor <victor@morel.ovh>
2019-10-20 19:14:39 +0200

    Ajout des fichiers crée avant git

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>
+            Accueil | Victor
+        </title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+
+    </body>
+</html>
